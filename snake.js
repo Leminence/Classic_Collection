23c33fb5b07b9f6bfe0ab5cd42d67ad86e43ab30
lema.repository@gmail.com <lema.repository@gmail.com>
2023-02-03 14:18:55 -0300

    v1

--- snake.js
+++ snake.js
@@ -36,13 +36,13 @@ function right() {
 }
 // Keys de movimento (desktop)
 document.addEventListener("keydown", function(event) {
-   if (event.key == "ArrowUp" || event.key === 87) {
+   if (event.keyCode == "ArrowUp" || event.keyCode == 87) {
       up();
-   } else if (event.key == "ArrowDown" || event.key === 83 ) {
+   } else if (event.keyCode == "ArrowDown" || event.keyCode == 83 ) {
       down();
-   } else if (event.key == "ArrowLeft" || event.key === 65) {
+   } else if (event.keyCode == "ArrowLeft" || event.keyCode == 65) {
       left();
-   } else if (event.key == "ArrowRight" || event.key === 68) {
+   } else if (event.keyCode == "ArrowRight" || event.keyCode == 68) {
       right();
    }
 });
